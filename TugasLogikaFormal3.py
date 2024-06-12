c5de21979294e2f1d27ec91d22b39e4681f64e94
LAB BIGDATA <you@example.com>
2024-06-12 20:53:27 +0700

    Final

--- TugasLogikaFormal3.py
+++ TugasLogikaFormal3.py
@@ -18,12 +18,12 @@ def ramsey_theorem(P, M, B):
     def all_blue(S):
         return And([Not(edges[(i, j)]) for i, j in combinations(S, 2)])
 
-    # Fungsi untuk mengecek apakah terdapat klik merah Km
+    # Fungsi untuk mengecek apakah terdapat clique merah Km
     def exist_red_clique(N, M):
         for subset in combinations(N, M):
             solver.add(Not(all_red(subset)))
 
-    # Fungsi untuk mengecek apakah terdapat klik biru Kb
+    # Fungsi untuk mengecek apakah terdapat clique biru Kb
     def exist_blue_clique(N, B):
         for subset in combinations(N, B):
             solver.add(Not(all_blue(subset)))
@@ -31,7 +31,7 @@ def ramsey_theorem(P, M, B):
     # Membuat daftar Node dengan range P
     N = list(range(P))
 
-    # Menambahkan constraint untuk mengecek apakah ada klik merah Km dan klik biru Kb
+    # Menambahkan constraint untuk mengecek apakah ada clique merah Km dan clique biru Kb
     exist_red_clique(N, M)
     exist_blue_clique(N, B)
 
